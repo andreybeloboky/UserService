881e48c4bfebebc0f2aaf87074fe3ac3b2ffa715
Andrey_Beloboky <andrei.beloboky@yandex.ru>
2026-08-04 14:22:47 +0300

    edit specification

--- src/main/java/com/beloboki/specification/UserSpecifications.java
+++ src/main/java/com/beloboki/specification/UserSpecifications.java
@@ -6,12 +6,21 @@ import org.springframework.data.jpa.domain.Specification;
 public class UserSpecifications {
 
     public static Specification<User> hasName(String name) {
-        return ((root, query, criteriaBuilder) -> criteriaBuilder.equal(root.get("name"), name));
+        return (root, query, criteriaBuilder) -> {
+            if (name == null || name.trim().isEmpty()) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("name"), name);
+        };
     }
 
     public static Specification<User> hasSurname(String surname) {
-        return ((root, query, criteriaBuilder) ->
-                criteriaBuilder.equal(root.get("surname"), surname));
+        return (root, query, criteriaBuilder) -> {
+            if (surname == null || surname.trim().isEmpty()) {
+                return null;
+            }
+            return criteriaBuilder.equal(root.get("surname"), surname);
+        };
     }
 
     private UserSpecifications() {}
